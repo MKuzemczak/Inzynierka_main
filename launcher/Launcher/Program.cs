f36ef46679425f0c3226dd47d0119773752a2fd1
Michał Kuzemczak <kuzemczak.michal@gmail.com>
2020-11-29 16:35:29 +0100

    Launcher

--- launcher/Launcher/Program.cs
+++ launcher/Launcher/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System.Diagnostics;
+using System.Threading;
 
 namespace Launcher
 {
     class Program
     {
+        private static string IncomingQueueName { get; } = "inzynierka_launcher";
+        private static string AppQueueName { get; } = "inzynierka_app";
+        private static string PythonQueueName { get; } = "inzynierka_python";
+        private static RabbitMQCommunicationService Communicator { get; set; }
+        private static AutoResetEvent AppClose = new AutoResetEvent(false);
+
+        static void InitializeCommunicator()
+        {
+            Communicator = RabbitMQCommunicationService.Instance;
+            Communicator.Initialize();
+            Communicator.DeclareIncomingQueue(IncomingQueueName);
+            Communicator.MessageReceived += MessageReceiver;
+        }
+
         static void Main(string[] args)
         {
+            InitializeCommunicator();
+
             Process pythonControllerProcess = new Process();
             pythonControllerProcess.StartInfo.FileName = "pythonw";
 
@@ -17,6 +34,30 @@ namespace Launcher
             pythonControllerProcess.StartInfo.UseShellExecute = false;
             pythonControllerProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
             pythonControllerProcess.Start();
+
+            AppClose.WaitOne();
+        }
+
+        private static void MessageReceiver(object sender, MessageReceivedEventArgs e)
+        {
+            if (e.QueueName != IncomingQueueName)
+                return;
+
+            if (e.Message == "{\"name\": \"SetupFinishedIndication\", \"sender\": \"inzynierka_python\", \"receiver\": \"inzynierka_launcher\", \"contents\": []}")
+            {
+                Process appProcess = new Process();
+                appProcess.StartInfo.FileName = "Inzynierka.exe";
+                appProcess.StartInfo.Arguments = "";
+                appProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+
+                // Comment this line, if you want to launch piceon yourself
+                // from Visual studio
+                appProcess.Start();
+
+                AppClose.Set();
+            }
+
+            return;
         }
     }
 }
